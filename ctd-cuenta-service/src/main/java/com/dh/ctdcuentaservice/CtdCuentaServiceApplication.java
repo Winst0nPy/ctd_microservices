40c274478c34b1dd7c6202fb1f4e0bf28631a68a
Daniel Cagliari <dacagliari@gmail.com>
2022-08-21 20:36:47 -0300

    adding new service with dev profile

---
+++ ctd-cuenta-service/src/main/java/com/dh/ctdcuentaservice/CtdCuentaServiceApplication.java
@@ -0,0 +1,41 @@
+package com.dh.ctdcuentaservice;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootApplication
+public class CtdCuentaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CtdCuentaServiceApplication.class, args);
+    }
+
+}
+
+@RestController
+class AccountService{
+
+    @Value("${message}")
+    private String message;
+
+    @Value("${global-message}")
+    private String globalMessage;
+
+    @RequestMapping(method = RequestMethod.GET, path="service")
+    public Map<String, String> message(){
+        Map<String, String> response = new HashMap<>();
+        response.put("message", message);
+        response.put("global-message", globalMessage);
+        return response;
+    }
+
+
+
+}
---
+++ ctd-cuenta-service/src/test/java/com/dh/ctdcuentaservice/CtdCuentaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dh.ctdcuentaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtdCuentaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
